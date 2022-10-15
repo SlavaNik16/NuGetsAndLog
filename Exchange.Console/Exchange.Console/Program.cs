5eab7443930c786fd19981eba1585c2bf6cfc9a4
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2022-10-15 15:22:18 +0300

    Add files via upload

---
+++ Exchange.Console/Exchange.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Exchange.Core;
+namespace Exchange.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            var exchange = new ExchangeCore(65.11m);
+            var result =  exchange.Cal(70); 
+            Console.WriteLine($"{result:C2}");
+
+        }
+    }
+}
---
+++ Exchange.Core/Exchange.Core/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace Exchange.Core
+{
+    /// <summary>
+    /// Ядро обменника(библиотека) 
+    /// </summary>
+    public class ExchangeCore
+    {
+        private readonly decimal Course;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public ExchangeCore(decimal Course)
+        {
+            this.Course = Course;
+        }
+        /// <summary>
+        /// Вычисляет значение по раннее введеному курсу
+        /// </summary>
+
+        public decimal Cal(decimal money) => Course * money;
+    }
+}
