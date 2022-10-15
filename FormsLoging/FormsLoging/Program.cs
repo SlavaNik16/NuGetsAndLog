85fe12629bb714988a88b87081fa8f2c91cc0a50
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2022-10-15 15:21:27 +0300

    Add files via upload

---
+++ FormsLoging/FormsLoging/Program.cs
@@ -0,0 +1,28 @@
+﻿using Serilog;
+using System;
+using System.Windows.Forms;
+
+namespace FormsLoging
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var assembly = System.Reflection.Assembly.GetCallingAssembly();
+            var version = assembly.GetName().Version.ToString();
+            Log.Logger = new LoggerConfiguration()
+
+                .Enrich.WithProperty("Version", version)
+                .WriteTo.File("log-.txt", rollingInterval: RollingInterval.Day,outputTemplate:
+              "{Timestamp:yyyy:MM:dd HH:mm} [{Level}] №[{Version}] {Message}{NewLine}{Exception}").CreateLogger();
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
